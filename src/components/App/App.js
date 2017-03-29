d8ffad375db6acad69cf4e47300239d24c3be9d7
Illia Shevchenko <illyashevchenko@gmail.com>
2017-03-29 11:32:04 +0200

    Refactor css + move to BEM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,11 +9,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+        <div className="App__header">
+          <img src={logo} className="App__logo" alt="logo"/>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="App__intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
