13280c7da2840e0d15502e76d5b487aeaf93d111
Illia Shevchenko <illyashevchenko@gmail.com>
2017-03-31 14:25:09 +0200

    Add logic for ActionInput

--- src/components/controls/ActionInput/ActionInput.js
+++ src/components/controls/ActionInput/ActionInput.js
@@ -1,29 +1,70 @@
-import React, { PureComponent } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './ActionInput.css';
 
 import Button from '../Button/Button.js'
 
-class ActionInput extends PureComponent {
+class ActionInput extends Component {
   constructor(props) {
     super(props);
 
     this.onClick = this.onClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyEvent = this.handleKeyEvent.bind(this);
+
     this.buttonSiblingOn = ['left'];
+    this.state = { value: '' };
   }
 
   onClick() {
+    this.act();
+  }
 
+  act() {
+    this.props.onAct(this.state.value);
+    this.clear();
+  }
+
+  clear() {
+    this.setState({ value: '' });
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleKeyEvent(event) {
+    switch (event.key) {
+      case 'Escape':
+        this.clear();
+        break;
+      case 'Enter':
+        this.act();
+        break;
+      default:
+    }
   }
 
   render() {
+    const { placeholder, actionTitle } = this.props;
+
     return (
       <div className="ActionInput">
-        <input className="ActionInput__input"/>
+        <input className="ActionInput__input"
+               placeholder={ placeholder }
+               value={ this.state.value }
+               onChange={ this.handleChange }
+               onKeyDown={ this.handleKeyEvent }/>
         <Button className="ActionInput__button"
                 siblingOn={ this.buttonSiblingOn }
-                onClick={ this.onClick }>Add</Button>
+                onClick={ this.onClick }>{ actionTitle }</Button>
       </div>
     );
+  }
+
+  static propTypes = {
+    placeholder: PropTypes.string,
+    actionTitle: PropTypes.string.isRequired,
+    onAct: PropTypes.func.isRequired,
   }
 }
 
--- src/components/items/CategoryList/CategoryList.js
+++ src/components/items/CategoryList/CategoryList.js
@@ -7,7 +7,9 @@ import ActionInput from '../../controls/ActionInput/ActionInput.js';
 class CategoryList extends PureComponent {
   render() {
     return <div className="CategoryList">
-      <ActionInput />
+      <ActionInput placeholder="Enter category title"
+                   actionTitle="Add"
+                   onAct={ (value) => console.log('Category to add: ', value) }/>
       <ItemsList />
     </div>;
   }
