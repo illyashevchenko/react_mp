718ef251332a02bf1ac43759fcd32e1bad13cd0f
Illia Shevchenko <illyashevchenko@gmail.com>
2017-04-01 18:55:35 +0200

    Build data and flow for it for app/main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,60 @@ import MainPage from '../pages/Main/Main.js';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.init();
+
+    this.setCategories = this.setCategories.bind(this);
+    this.setTasks = this.setTasks.bind(this);
+    this.selectCategory = this.selectCategory.bind(this);
+  }
+
+  init() {
+    this.state = {
+      category: null,
+      categories: [
+        { title: 'Category1', id: 1 },
+        { title: 'Category2', id: 2 },
+        {
+          title: 'Category3', id: 3,
+          sub: [],
+        },
+      ],
+      tasks: [
+        { title: 'TODO #1', done: false, id: 101, category: 3 },
+        { title: 'TODO #2', done: false, id: 102, category: 2 },
+        { title: 'TODO #3', done: false, id: 103, category: 1 },
+        { title: 'TODO #4', done: false, id: 104, category: 3 },
+        { title: 'TODO #5', done: true, id: 105, category: 3 },
+        { title: 'TODO #6', done: true, id: 106, category: 3 },
+        { title: 'TODO #7', done: false, id: 107, category: 1 },
+      ],
+    };
+  }
+
+  setCategories(categories) {
+    this.setState({ categories });
+  }
+
+  setTasks(tasks) {
+    this.setState({ tasks });
+  }
+
+  selectCategory(category) {
+    this.setState({ category });
+  }
+
   render() {
     return (
       <div className="App">
-        <MainPage/>
+        <MainPage
+          tasks={ this.state.tasks }
+          categories={ this.state.categories }
+          category={ this.state.category }
+          setCategories={ this.setCategories }
+          setTasks={ this.setTasks }
+          selectCategory={ this.selectCategory }/>
       </div>
     );
   }
--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, PropTypes } from 'react';
 import './Main.css';
 
 import Header from '../../page-elements/Header/Header.js';
@@ -9,16 +9,58 @@ import TwoRows from '../../layouts/TwoRows/TwoRows.js';
 
 
 class Main extends PureComponent {
+  createCategoryList() {
+    return (
+      <CategoryList
+        list={ this.props.categories }
+        actions={{
+          set: this.props.setCategories,
+          select: this.props.selectCategory,
+        }}/>
+    );
+  }
+
+  createTodoList() {
+    return (
+      <TodoList
+        list={ this.props.tasks }
+        activeCategory={ this.props.category }
+        filter={ { search: '', done: false } }
+        actions={{
+          set: this.props.setTasks,
+        }}/>
+    );
+  }
+
+  getCompleted() {
+    const completed = this.props.tasks.map((task) => task.done);
+    const all = this.props.tasks.length;
+
+    return Math.round(completed / all);
+  }
+
   render() {
     return (
       <div className="Main">
         <Header text="To-Do List"/>
-        <ProgressBar complete="15"/>
-        <TwoRows left={ <CategoryList /> }
-                 right={ <TodoList /> }/>
+        <ProgressBar complete={ this.getCompleted()}/>
+        <TwoRows
+          left={ this.createCategoryList() }
+          right={ this.createTodoList() }/>
       </div>
     );
   }
 }
+
+
+Main.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.object).isRequired,
+  category: PropTypes.object.isRequired,
+  tasks: PropTypes.arrayOf(PropTypes.object).isRequired,
+
+  setCategories: PropTypes.func.isRequired,
+  selectCategory: PropTypes.func.isRequired,
+  setTasks: PropTypes.func.isRequired,
+};
 
 export default Main;
