1006dc23b96d3e68d98604fee3e61bae1621c30a
Illia Shevchenko <illyashevchenko@gmail.com>
2017-04-09 14:43:52 +0200

    Create ToDoFilter component

---
+++ src/components/page-elements/ToDoFilter/ToDoFilter.js
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+import './ToDoFilter.css';
+
+// TODO: refactor this
+const setItem = (set, filter, prop, eventProp) => // eslint-disable-line max-params
+  (event) =>
+    set(Object.assign({}, filter, {
+      [prop]: event.target[eventProp],
+    }));
+
+
+const ToDoFilter = ({ filter, actions: { set } }) =>
+  <div className="ToDoFilter">
+    <label className="ToDoFilter__block">
+      <input
+        type="checkbox"
+        className="ToDoFilter__checkbox"
+        value={ filter.onlyDone }
+        onChange={ setItem(set, filter, 'search', 'value') }/>
+      Show done
+    </label>
+    <input
+      className="ToDoFilter__block ToDoFilter__input"
+      placeholder="Search ..."
+      value={ filter.search }
+      onChange={ setItem(set, filter, 'onlyDone', 'checked') }/>
+  </div>;
+
+ToDoFilter.PropTypes = {
+  filter: PropTypes.shape({
+    search: PropTypes.string,
+    onlyDone: PropTypes.bool,
+  }).isRequired,
+  actions: PropTypes.shape({
+    set: PropTypes.func.isRequired,
+  }),
+};
+
+export default ToDoFilter;
