5a4bf8b517ef051ae20fe678cdc281e996b8dab1
Illia Shevchenko <illyashevchenko@gmail.com>
2017-04-09 14:44:10 +0200

    Add using of TODOFilter

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -6,11 +6,25 @@ const { completedPercentage } = actions;
 
 import Header from '../../page-elements/Header/Header.js';
 import ProgressBar from '../../page-elements/ProgressBar/ProgressBar.js';
+import ToDoFilter from '../../page-elements/ToDoFilter/ToDoFilter.js';
 import CategoryList from '../../items/CategoryList/CategoryList.js';
 import TodoList from '../../items/TodoList/TodoList.js';
 import TwoRows from '../../layouts/TwoRows/TwoRows.js';
 
 class Main extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = { filter: { search: '', onlyDone: false } };
+    this.filterActions = {
+      set: this.setFilter.bind(this),
+    };
+  }
+
+  setFilter(filter) {
+    this.setState({ filter });
+  }
+
   createCategoryList() {
     const { categories, category, setCategories: set, selectCategory: select } = this.props;
     return (
@@ -27,7 +41,7 @@ class Main extends PureComponent {
       <TodoList
         tasks={ tasks }
         activeCategory={ category }
-        filter={ { search: '', onlyDone: false } }
+        filter={ this.state.filter }
         actions={ { set } }/>
     );
   }
@@ -36,7 +50,11 @@ class Main extends PureComponent {
     return (
       <div className="Main page">
         <div className="page__section-static">
-          <Header text="To-Do List"/>
+          <Header text="To-Do List">
+            <ToDoFilter
+              filter={ this.state.filter }
+              actions={ this.filterActions }/>
+          </Header>
           <ProgressBar complete={ completedPercentage(this.props.tasks)}/>
         </div>
         <TwoRows
